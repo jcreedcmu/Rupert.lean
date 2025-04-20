4c743880829d32c5fdab815c8b78427c07a911a6
David Renshaw <dwrenshaw@gmail.com>
2025-04-20 16:18:54 -0400

    fix typo

--- Rupert.lean
+++ Rupert.lean
@@ -10,7 +10,7 @@ abbrev SO3 := Matrix.specialOrthogonalGroup (Fin 3) ℝ
 /-- Projects a vector from `ℝ³` to `ℝ²` by ignoring the third coordinate. -/
 def project32 (v : ℝ³) : ℝ² := ![v 0, v 1]
 
-/-- The Rupert Propert for a convex polyhedron given as a set of vertices. -/
+/-- The Rupert Property for a convex polyhedron given as a set of vertices. -/
 def IsRupert (p : Set ℝ³) : Prop :=
    ∃ inner_rot ∈ SO3, ∃ outer_rot ∈ SO3, ∃ inner_offset : ℝ²,
    let inner_shadow := Set.image (λ t ↦ inner_offset + project32 (inner_rot *ᵥ t)) p
