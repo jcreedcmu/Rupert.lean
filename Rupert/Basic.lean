368c417cf88d940aa97f9dfdd769ded5bb5a32a6
David Renshaw <dwrenshaw@gmail.com>
2025-04-23 08:43:48 -0400

    use notation for Set.image

--- Rupert/Basic.lean
+++ Rupert/Basic.lean
@@ -13,8 +13,8 @@ def project32 (v : ℝ³) : ℝ² := ![v 0, v 1]
 /-- The Rupert Property for a convex polyhedron given as a set of vertices. -/
 def IsRupert (p : Set ℝ³) : Prop :=
    ∃ inner_rot ∈ SO3, ∃ outer_rot ∈ SO3, ∃ inner_offset : ℝ²,
-   let inner_shadow := Set.image (λ t ↦ inner_offset + project32 (inner_rot *ᵥ t)) p
-   let outer_shadow := Set.image (λ t ↦ project32 (outer_rot *ᵥ t)) p
+   let inner_shadow := (λ t ↦ inner_offset + project32 (inner_rot *ᵥ t)) '' p
+   let outer_shadow := (λ t ↦ project32 (outer_rot *ᵥ t)) '' p
    closure inner_shadow ⊆ interior (convexHull ℝ outer_shadow)
 
 lemma closure_eq_of_finite {X : Type*} [TopologicalSpace X] [T1Space X]
