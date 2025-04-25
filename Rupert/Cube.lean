bf299e8b7796da97aa07a04bb57e6a56f1970911
Jason Reed <jcreed@gmail.com>
2025-04-25 15:22:36 -0400

    Fix the angle being wrong

--- Rupert/Cube.lean
+++ Rupert/Cube.lean
@@ -16,7 +16,7 @@ def cube : Fin 8 → ℝ³ := ![
 
 section rotations
 open Real
-
+open Matrix
 -- Rotate by just enough about the y-axis to bring (1, 0, √2/2) to (0, 0, z) for some z.
 -- Take cos⁻¹ to find out the angle.
 def outer_rot2 : Matrix.specialOrthogonalGroup (Fin 3) ℝ :=
@@ -25,13 +25,21 @@ def outer_rot2 : Matrix.specialOrthogonalGroup (Fin 3) ℝ :=
 -- Rotate 45° by x-axis
 noncomputable
 def outer_rot1 : Matrix.specialOrthogonalGroup (Fin 3) ℝ :=
- let q : Quaternion ℝ := ⟨cos (π/4), sin (π/4), 0, 0⟩
- let qnz : q.normSq ≠ 0 := by
+ let q : Quaternion ℝ := ⟨cos (π/8), sin (π/8), 0, 0⟩
+ have qnz : q.normSq ≠ 0 := by
    rw [Quaternion.normSq_def']
-   simp only [cos_pi_div_four, sin_pi_div_four, ne_eq, OfNat.ofNat_ne_zero,
-     not_false_eq_true, zero_pow, add_zero, add_self_eq_zero, pow_eq_zero_iff, div_eq_zero_iff,
-     Nat.ofNat_nonneg, sqrt_eq_zero, or_self, q]
+   simp only [q, ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true, zero_pow,
+              add_zero, cos_sq_add_sin_sq, one_ne_zero]
  let M := matrix_of_quat q
+ let res := M *ᵥ ![1,1,1]
+ have : res = ![1, √2, 0] := by
+   simp only [res, M, matrix_of_quat, q, Matrix.mulVec]
+   simp [Matrix.of_apply]
+   ext i; fin_cases i
+   · simp only [Fin.zero_eta, Fin.isValue, Pi.add_apply, Function.comp_apply, cons_val_zero,
+     head_cons, tail_cons, add_zero]; sorry
+   · sorry
+   · sorry
  ⟨M, matrix_of_quat_is_s03 q qnz⟩
 
 noncomputable
