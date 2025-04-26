3407d0b73669ec1c7f7ae6da6cc3125f78393099
Jason Reed <jcreed@gmail.com>
2025-04-26 16:56:07 -0400

    Did I learn anything from this? Idk.

--- Rupert/Sandbox2.lean
+++ Rupert/Sandbox2.lean
@@ -19,29 +19,41 @@ def cvt : Church →  ChurchK := by
 def ChurchL : Type 1 := (A : Type)
   → ((A → A) → A) → A
 
-def ChurchL2  : Type 1 := (A : Fin 2 → Type)
-  → (n : Fin 2) → ((∀ m ≥ n, (∀ p ≥ m, A p → A p) → A m) → A n)
+def ChurchL3  : Type 1 := (A : Fin 3 → Type)
+  → (n : Fin 3) → ((∀ m ≥ n, (∀ p ≥ m, A p → A p) → A m) → A n)
 
-lemma above_1 (P : Fin 2 → Type) : (∀ m ≥ 1, P m) = P 1 := by
+lemma above_2 (P : Fin 3 → Type) : (∀ m ≥ 2, P m) = P 2 := by
   sorry
-def above_0 (P : Fin 2 → Type) : (∀ m ≥ 0, P m) = (P 0 × P 1) := by
+lemma above_1 (P : Fin 3 → Type) : (∀ m ≥ 1, P m) = (P 1 × P 2) := by
+  sorry
+def above_0 (P : Fin 3 → Type) : (∀ m ≥ 0, P m) = (P 0 × P 1 × P 2) := by
   sorry
 
-def cvtL : ChurchL → ChurchL2 := by
+def cvtL : ChurchL → ChurchL3 := by
   intro t A n
   match n with
   | ⟨0, _⟩ =>
      change (∀ m ≥ 0, (∀ p ≥ m, A p → A p) → A m) → A 0
-     rw [above_0, above_0, above_1]
-     intro ⟨x, y⟩
-     have t0 := t (A 0)
-     have t1 := t (A 1)
-     have a1 := t1 y
-     apply t0
-     intro f0
-     apply x
-     exact ⟨f0, fun _ => a1⟩
+     rw [above_0, above_0, above_1, above_2]
+     intro ⟨x0, ⟨x1, x2⟩⟩
+     let a2 := t (A 2) x2
+     let z1 : (A 1 → A 1) → A 1 := fun f1 => x1 ⟨f1, fun _ => a2⟩
+     let a1 := t (A 1) z1
+     let z0 : (A 0 → A 0) → A 0 := fun f0 => x0 ⟨f0, fun _ => a1, fun _ => a2⟩
+     let a0 := t (A 0) z0
+     exact a0
   | ⟨1, _⟩ =>
     change (∀ m ≥ 1, (∀ p ≥ m, A p → A p) → A m) → A 1
-    rw [above_1, above_1]
-    exact t (A 1)
+    rw [above_1, above_1, above_2]
+    intro ⟨x, y⟩
+    have t0 := t (A 1)
+    have t1 := t (A 2)
+    have a1 := t1 y
+    apply t0
+    intro f0
+    apply x
+    exact ⟨f0, fun _ => a1⟩
+  | ⟨2, _⟩ =>
+    change (∀ m ≥ 2, (∀ p ≥ m, A p → A p) → A m) → A 2
+    rw [above_2, above_2]
+    exact t (A 2)
