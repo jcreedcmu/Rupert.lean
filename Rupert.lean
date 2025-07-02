0ebb97b038d0fc69d5b1361d88955aaff8d22c88
David Renshaw <dwrenshaw@gmail.com>
2025-07-02 10:21:33 -0400

    delete duplicated command

--- Rupert.lean
+++ Rupert.lean
@@ -26,13 +26,6 @@ info: 'Cube.rupert' depends on axioms: [propext, Classical.choice, Quot.sound]
 
 
 /--
-info: 'TriakisTetrahedron.rupert' depends on axioms: [propext, Classical.choice, Quot.sound]
--/
-#guard_msgs in
-#print axioms TriakisTetrahedron.rupert
-
-
-/--
 info: 'Tetrahedron.rupert' depends on axioms: [propext, Classical.choice, Quot.sound]
 -/
 #guard_msgs in
