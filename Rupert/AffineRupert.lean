0c6667693734097f0c17bb02fe1c87a6bc07fff2
Jason Reed <jcreed@gmail.com>
2025-07-10 16:38:10 -0400

    Tidying

--- Rupert/AffineRupert.lean
+++ Rupert/AffineRupert.lean
@@ -11,21 +11,25 @@ import Mathlib
     By "comfortably" we mean the closure of one set is a subset of the
     interior of the other. This definition rules out trivial cases of
     a set fitting inside itself. -/
-def IsAffineRupertPair {P : Type*} {V : Type*} [MetricSpace P] [NormedAddCommGroup V]
+def IsAffineRupertPairForSubspace {P : Type*} {V : Type*} [MetricSpace P] [NormedAddCommGroup V]
     [InnerProductSpace ℝ V] [NormedAddTorsor V P] [FiniteDimensional ℝ V]
-    (inner outer : Set P) : Prop :=
-    ∃ (inner_isometry outer_isometry : AffineIsometry ℝ P P)
-      (Q : AffineSubspace ℝ P) (_ : Nonempty Q) (_ : IsCoatom Q),
-
-
+    (inner outer : Set P) (Q : AffineSubspace ℝ P) [Nonempty Q] (_ : IsCoatom Q) : Prop :=
+    ∃ (inner_isometry outer_isometry : AffineIsometry ℝ P P),
     let proj := EuclideanGeometry.orthogonalProjection Q
     let inner_shadow := (proj ∘ inner_isometry) '' inner
     let outer_shadow := (proj ∘ outer_isometry) '' outer
     closure inner_shadow ⊆ interior outer_shadow
 
-/-- The Rupert Property for a subset S of affine space P. S has the Rupert property
-    if it has the pairwise Rupert property with respect to itself. -/
+/-- A pair of subsets X, Y are Rupert if there exists a subspace satisfying
+    the above definition. -/
+def IsAffineRupertPair {P : Type*} {V : Type*} [MetricSpace P] [NormedAddCommGroup V]
+    [InnerProductSpace ℝ V] [NormedAddTorsor V P] [FiniteDimensional ℝ V]
+    (inner outer : Set P) : Prop :=
+    ∃ (Q : AffineSubspace ℝ P) (_ : Nonempty Q) (Qca : IsCoatom Q),
+    IsAffineRupertPairForSubspace inner outer Q Qca
+
+/-- A single subset X is Rupert if the pair X, X is Rupert -/
 def IsAffineRupertSet  {P : Type*} {V : Type*} [MetricSpace P] [NormedAddCommGroup V]
     [InnerProductSpace ℝ V] [NormedAddTorsor V P] [FiniteDimensional ℝ V]
-    (S : Set P) : Prop :=
-    IsAffineRupertPair S S
+    (X : Set P) : Prop :=
+    IsAffineRupertPair X X
--- Rupert/LinearRupert.lean
+++ Rupert/LinearRupert.lean
@@ -1,27 +1,35 @@
 import Mathlib
 
 /-- The Rupert Property for a pair of subsets X, Y of an arbitrary
-    finite-dimensional real vector space P. X has the Rupert property
-    with respect to Y if there exist
-    - linear isometries transforming X and Y respectively
-    - an maximal nontrivial linear subspace Q of P
-    such that the orthogonal projection onto Q of the transformed image of X fits
-    "comfortably" within the projection onto Q of the transformed image of Y.
+    finite-dimensional real vector space P with chosen subspace Q. X
+    has the Rupert property with respect to Y and the maximal nonempty
+    subspace Q if there exist - linear isometries transforming X and Y
+    respectively such that the orthogonal projection onto Q of the
+    transformed image of X fits "comfortably" within the projection
+    onto Q of the transformed image of Y.
 
     By "comfortably" we mean the closure of one set is a subset of the
     interior of the other. This definition rules out trivial cases of
     a set fitting inside itself. -/
-def IsLinearRupertPair {P : Type*} [NormedAddCommGroup P] [InnerProductSpace ℝ P] [FiniteDimensional ℝ P]
-    (inner outer : Set P) : Prop :=
-    ∃ (inner_isometry outer_isometry : P →ₗᵢ[ℝ] P)
-      (Q : Submodule ℝ P) (_ : Nonempty Q) (_ : IsCoatom Q),
+def IsLinearRupertPairForSubspace {P : Type*} [NormedAddCommGroup P]
+    [InnerProductSpace ℝ P] [FiniteDimensional ℝ P]
+    (inner outer : Set P) (Q : Submodule ℝ P) [Nonempty Q] (_ : IsCoatom Q) : Prop :=
+    ∃ (inner_isometry outer_isometry : P →ₗᵢ[ℝ] P),
     let proj := Submodule.orthogonalProjection Q
     let inner_shadow : Set Q := (proj ∘ inner_isometry : P → Q) '' inner
     let outer_shadow : Set Q := (proj ∘ outer_isometry : P → Q) '' outer
     closure inner_shadow ⊆ interior outer_shadow
 
-/-- The Rupert Property for a subset S of linear space P. S has the Rupert property
-    if it has the pairwise Rupert property with respect to itself. -/
-def IsLinearRupertSet {P : Type*} [NormedAddCommGroup P] [InnerProductSpace ℝ P] [FiniteDimensional ℝ P]
-    (S : Set P) : Prop :=
-    IsLinearRupertPair S S
+/-- A pair of subsets X, Y are Rupert if there exists a subspace satisfying
+    the above definition. -/
+def IsLinearRupertPair {P : Type*} [NormedAddCommGroup P]
+    [InnerProductSpace ℝ P] [FiniteDimensional ℝ P]
+    (inner outer : Set P) : Prop :=
+    ∃ (Q : Submodule ℝ P) (_ : Nonempty Q) (Qca : IsCoatom Q),
+    IsLinearRupertPairForSubspace inner outer Q Qca
+
+/-- A single subset X is Rupert if the pair X, X is Rupert -/
+def IsLinearRupertSet {P : Type*} [NormedAddCommGroup P]
+    [InnerProductSpace ℝ P] [FiniteDimensional ℝ P]
+    (X : Set P) : Prop :=
+    IsLinearRupertPair X X
