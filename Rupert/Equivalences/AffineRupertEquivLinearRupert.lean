4cfdc2e910b3eb664740999b4751e287477a7768
Jason Reed <jcreed@gmail.com>
2025-07-10 18:36:51 -0400

    Great forward progress

--- Rupert/Equivalences/AffineRupertEquivLinearRupert.lean
+++ Rupert/Equivalences/AffineRupertEquivLinearRupert.lean
@@ -5,16 +5,36 @@ import Rupert.LinearRupert
 import Rupert.Equivalences.ProjectionLemmas
 open Matrix
 
+-- Thanks to Jireh Loreaux!
+-- https://leanprover.zulipchat.com/#narrow/channel/113489-new-members/topic/Orthogonal.20projection.20commutes.20with.20affine.20space.20inclusion/with/527494321
+theorem affine_projection_eq_linear_projection {P : Type*} [NormedAddCommGroup P]
+    [InnerProductSpace ℝ P] [FiniteDimensional ℝ P]
+    (Q : Submodule ℝ P) [Nonempty Q] (p : P) :
+  Q.orthogonalProjection p = EuclideanGeometry.orthogonalProjection Q.toAffineSubspace p := by
+    ext1
+    apply Set.eq_of_mem_singleton
+    rw [← EuclideanGeometry.inter_eq_singleton_orthogonalProjection]
+    apply Set.mem_inter (by simp)
+    simp only [Submodule.toAffineSubspace_direction, SetLike.mem_coe, AffineSubspace.mem_mk', vsub_eq_sub]
+    rw [← neg_mem_iff, neg_sub]
+    simp
 
 theorem linear_for_subspace_imp_affine_for_subspace {P : Type*} [NormedAddCommGroup P]
     [InnerProductSpace ℝ P] [FiniteDimensional ℝ P] (X Y : Set P)
     (Q : Submodule ℝ P) [Nonempty Q] :
     IsLinearRupertPairForSubspace X Y Q → IsAffineRupertPairForSubspace X Y (Q.toAffineSubspace) := by
     intro ⟨ Xi, Yi, hsub ⟩
-    let Xs := (Q.orthogonalProjection ∘ Xi) '' X
-    let Ys := (Q.orthogonalProjection ∘ Yi) '' Y
-    change closure Xs ⊆ interior Ys at hsub
     use Xi.toAffineIsometry, Yi.toAffineIsometry
     let aproj := EuclideanGeometry.orthogonalProjection Q.toAffineSubspace
+
+    have Xe : Q.orthogonalProjection ∘ Xi = aproj ∘ Xi := by
+     ext1 p; change Q.orthogonalProjection (Xi p) = aproj (Xi p)
+     rw [affine_projection_eq_linear_projection Q (Xi p)]
+
+    have Ye : Q.orthogonalProjection ∘ Yi = aproj ∘ Yi := by
+     ext1 p; change Q.orthogonalProjection (Yi p) = aproj (Yi p)
+     rw [affine_projection_eq_linear_projection Q (Yi p)]
+
     change closure ((aproj ∘ Xi) '' X) ⊆ interior ((aproj ∘ Yi) '' Y)
-    sorry
+    rw [← Xe, ← Ye]
+    exact hsub
