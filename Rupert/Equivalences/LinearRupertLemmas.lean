7770275b4f9843c6d655d022f42196a197ce48d2
Jason Reed <jcreed@gmail.com>
2025-07-12 14:29:37 -0400

    Progress

--- Rupert/Equivalences/LinearRupertLemmas.lean
+++ Rupert/Equivalences/LinearRupertLemmas.lean
@@ -4,20 +4,56 @@ import Rupert.LinearRupert
 
 variable {P : Type*} [NormedAddCommGroup P] [InnerProductSpace ℝ P] [FiniteDimensional ℝ P] (X Y : Set P)
 
-theorem coatomic_subspaces_equivalent (Q1 Q2 : Submodule ℝ P) (_ : IsCoatom Q1) (_ : IsCoatom Q2) :
-    ∃ T : P →ₗᵢ[ℝ] P, Submodule.map T Q1 = Q2 := by
+-- #check exists_orthonormalBasis
+-- #check LinearEquiv.ofRankEq
+-- #check Orthonormal.exists_orthonormalBasis_extension_of_card_eq
+-- #check Submodule.Quotient.nontrivial_of_lt_top
+
+
+theorem coatomic_subspace_dim (Q : Submodule ℝ P) (Qcoatom : IsCoatom Q) :
+    Module.finrank ℝ P = Module.finrank ℝ Q + 1 := by
+  simp_all [IsCoatom]
+  obtain ⟨Qnontriv, Qmax⟩ := Qcoatom
+
+  -- FIXME: is there a better way?
+  have hne : Q.carrier ≠ Set.univ := fun a =>
+     False.elim (Qnontriv (Submodule.ext fun x ↦ Eq.to_iff (congrFun a x)))
+
+  -- x an element in P \ Q
+  let ⟨ x, hx ⟩ := (Set.ne_univ_iff_exists_notMem Q.carrier).mp hne
+  let n : ℕ := Module.finrank ℝ P
+  let nQ : ℕ := Module.finrank ℝ Q
+
+  have bP : Basis (Fin n) ℝ P := Module.finBasis ℝ P
+  have bQ : Basis (Fin nQ) ℝ Q := Module.finBasis ℝ Q
+
+  let extended : Option (Fin nQ) → P
+   | some v => bQ v
+   | none   => x
+  let Q' := Submodule.span ℝ (Set.range extended)
   sorry
 
-theorem linear_rupert_respects_subspace_iso (Q : Submodule ℝ P) [Nonempty Q]
-    (T : P →ₗᵢ[ℝ] P) :
+
+theorem coatomic_subspaces_equivalent (Q1 Q2 : Submodule ℝ P) (_ : IsCoatom Q1) (_ : IsCoatom Q2) :
+    ∃ T : P ≃ₗᵢ[ℝ] P, Submodule.map T Q1 = Q2 := by
+  sorry
+
+section nonempty_q
+variable (Q : Submodule ℝ P) [Nonempty Q]
+
+theorem linear_rupert_respects_subspace_iso (T : P ≃ₗᵢ[ℝ] P) :
     IsLinearRupertPairForSubspace X Y Q → IsLinearRupertPairForSubspace X Y (Submodule.map T Q) := by
   sorry
 
-theorem rupert_for_one_subspace_imp_for_all {P : Type*} [NormedAddCommGroup P]
-    [InnerProductSpace ℝ P] [FiniteDimensional ℝ P]
-    (X Y : Set P) (Q : Submodule ℝ P) [Nonempty Q] (Qcoatom : IsCoatom Q) :
+theorem linear_rupert_pair_imp_subspace (Qcoatom : IsCoatom Q) :
     IsLinearRupertPair X Y → IsLinearRupertPairForSubspace X Y Q := by
   intro ⟨ Q', Q'inh, Q'coatom, hrup ⟩
   let ⟨ T, hT ⟩ := coatomic_subspaces_equivalent Q' Q Q'coatom Qcoatom
   simp only [← hT]
   exact linear_rupert_respects_subspace_iso X Y Q' T hrup
+
+theorem linear_rupert_pair_iff_subspace (Qcoatom : IsCoatom Q) :
+    IsLinearRupertPair X Y ↔ IsLinearRupertPairForSubspace X Y Q :=
+  ⟨ linear_rupert_pair_imp_subspace X Y Q Qcoatom,
+    fun rup => ⟨ Q, inferInstance, Qcoatom, rup ⟩⟩
+end nonempty_q
